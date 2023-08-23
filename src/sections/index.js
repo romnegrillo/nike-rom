49c8ba4e9b9127e8bf39ff1c0f607e243d83cebb
Romulo Negrillo Jr <romnegrillo@gmail.com>
2023-08-23 15:36:48 +0800

    Finished Nav component. Added constant data and button component for Hero section later.

--- src/sections/index.js
+++ src/sections/index.js
@@ -1,6 +1,6 @@
 import CustomerReviews from './CustomerReviews';
 import Footer from './Footer';
-import Hero from './PopularProducts';
+import Hero from './Hero';
 import PopularProducts from './PopularProducts';
 import Services from './Services';
 import SpecialOffer from './SpecialOffer';
