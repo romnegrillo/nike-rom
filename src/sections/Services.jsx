cb1c6d10db01ae1400eae774b3437649b1e6e580
Romulo Negrillo Jr <romnegrillo@gmail.com>
2023-08-30 16:48:51 +0800

    Started and finished Services section.

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,5 +1,15 @@
+import ServicesCard from '../components/ServicesCard';
+
+import { services } from '../constants';
+
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section className="flex justify-center gap-8 max-container flex-wrap">
+      {services.map((service, index) => (
+        <ServicesCard key={index} service={service} />
+      ))}
+    </section>
+  );
 };
 
 export default Services;
