86c41476aac4a397ea0771d9f1ba7af21f820eeb
Romulo Negrillo Jr <romnegrillo@gmail.com>
2023-11-11 04:57:47 +0800

    Removed redundant layout of footer component.

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,19 +1,26 @@
 import { Routes, Route } from 'react-router-dom';
 
+import { Nav, Footer } from '../components/common';
 import { Home, Products, About, Contact, NotFound } from '../pages';
 
 // TODO: Make the routes more reusable by being dependent from the hard coded data.
 
 const AppRoutes = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/products" element={<Products />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <>
+      {/* Navbar component is present individually on each page as it has different styles for each */}
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
+      <Footer />
+    </>
   );
 };
 
